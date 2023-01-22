50cb5e2eb995104dc6bc9445836794ed67a324ac
OrGabay2 <gabayor2000@gmail.com>
2023-01-22 11:22:53 +0200

    fix the code

--- DefaultIO.cpp
+++ DefaultIO.cpp
@@ -4,42 +4,29 @@
 
 #include "DefaultIO.h"
 
-/*StandardIO::StandardIO()
-{}
 
 
-void StandardIO:: write(string text) {
-    cout << text;
-}
-string StandardIO::read(){
-    string s;
-    getline(cin, s);
-    return s;
-}
-
-string StandardIO::readFile(){
-    string s;
-    getline(cin, s);
-    return s;
-}*/
-
+// This function reads data from the socket and returns it as a string:
 string SocketIO::read(){
+    // read data from the socket into a buffer:
     char buffer [4096];
     int expectedDatalen = sizeof(buffer);
+    // converts the buffer to a string:
     bzero(buffer, expectedDatalen);
     if (recv(clientID, buffer, sizeof(buffer)-1, 0)) {
         string answer=string(buffer);
-        answer=answer.substr(0,answer.find("D"));
+        answer=answer.substr(0,answer.find("D")); // removes anything after the substring "D"
         return answer;
     } else {
         return "";
     }
 }
 
-
+// This function reads data from the socket and returns it as a string:
  string SocketIO::readFile(){
      char c=0;
      string s="";
+     // read data from the socket into a buffer:
      char buffer [4096]={0};
      int expectedDatalen = sizeof(buffer);
      bzero(buffer, expectedDatalen);
@@ -47,6 +34,7 @@ string SocketIO::read(){
      if (readBytes <= 0) {
          return s;
      } else {
+         // converts the buffer to a string and returns it:
          for (int i = 0; i < expectedDatalen-1; i++) {
              c = buffer[i];
              if (c == '\0') {
@@ -58,6 +46,7 @@ string SocketIO::read(){
      }
     return s;
 }
+// This function writes the given text to the socket:
 void SocketIO::write(string text){
     const char* txt=text.c_str();
     send(clientID,txt,strlen(txt),0);
